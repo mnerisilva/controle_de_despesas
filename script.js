5e11cd43f13264967187b81e1cc5b958a6139fcf
Marcelo Neri da Silva <marceloneri@gmail.com>
2021-11-01 01:04:33 -0300

    update

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-const dummyTransaction = [
+/*const dummyTransaction = [
   { id: 1, name: "Bolo de brigadeiro", amount: -20 },
   { id: 2, name: "Salário", amount: 300 },
   { id: 3, name: "Torta de frango", amount: -10 },
   { id: 3, name: "Violão", amount: -150 },
-];
+];*/
 
-const transactions = document.getElementById("transactions");
+const url =
+  "https://my-json-server.typicode.com/mnerisilva/fake-api/transacoes";
+
+fetch(url)
+  .then((resp) => resp.json())
+  .then((data) => {
+    data.map((item) => {
+      addTransactionIntoDOM(item);
+    });
+  });
+
+const ulTransactions = document.querySelector("#transactions");
 
 const addTransactionIntoDOM = (transaction) => {
-  transactions.innerHTML =
-    transactions.innerHTML +
-    `<li class="
-    ${valueCheck(transaction.amount) ? "plus" : "minus"}
-    ">${transaction.name} <span>- R$ ${
-      transaction.amount
-    }</span><button class="delete-btn">x</button></li>`;
-  console.log(transaction.name);
+  const operator = transaction.amount < 0 ? "-" : "+";
+  const CSSClass = transaction.amount < 0 ? "minus" : "plus";
+  const li = document.createElement("li");
+  li.classList.add(CSSClass);
+  li.innerHTML = `${transaction.name} <span>R$ ${Math.abs(
+    transaction.amount
+  )}${operator}</span><button class="delete-btn">x</button>`;
+  ulTransactions.append(li);
 };
-
-const valueCheck = (value) => {
-  if (value > 0) {
-    return true;
-  }
-};
-
-//console.log(dummyTransaction[0]);
-
-dummyTransaction.map((item) => {
-  addTransactionIntoDOM(item);
-});
